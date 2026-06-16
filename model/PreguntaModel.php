05802169fb75771c67224bff9974e7474eb7b162
LGava <lgavagnin074@alumno.unlam.edu.ar>
2026-06-16 16:24:16 -0300

    Agrego tipo de dificultad en las preguntas y temporizador en la partida

--- model/PreguntaModel.php
+++ model/PreguntaModel.php
@@ -18,8 +18,56 @@ class PreguntaModel
     public function buscarPreguntaSegunId($idPregunta){
         $sql = "SELECT * FROM preguntas WHERE id = ?";
         $resultado = $this->database->query($sql, [$idPregunta]);
-        return $resultado ?? null;
+        return $resultado[0] ?? null;
     }
 
+    public function sumarPreguntasEntregadas($idPregunta) {
+        $sql = "UPDATE preguntas
+            Set veces_entregada = veces_entregada + 1
+            WHERE id = ?";
 
+        $this->database->execute($sql, [$idPregunta]);
+    }
+
+    public function sumarPreguntasCorrectas($idPregunta) {
+        $sql = "UPDATE preguntas
+            SET veces_correcta =
+            veces_correcta + 1
+            WHERE id = ?";
+
+        $this->database->execute($sql, [$idPregunta]);
+    }
+
+    public function calcularDificultad($idPregunta) {
+        $pregunta = $this->buscarPreguntaSegunId($idPregunta);
+        $entregadas = $pregunta["veces_entregada"];
+        $correctas = $pregunta["veces_correcta"];
+
+        if($entregadas == 0){
+            return;
+        }
+        $porcentaje = ($correctas / $entregadas) * 100;
+
+        if($porcentaje >= 70){
+            $dificultad = "facil";
+        }else if($porcentaje >= 30){
+            $dificultad = "medio";
+        }else{
+            $dificultad = "dificil";
+        }
+
+        $sql = "UPDATE preguntas
+            SET dificultad = ?
+            WHERE id = ?";
+        
+        $this->database->execute($sql, [$dificultad, $idPregunta]);
+    }
+
+    public function buscarPreguntasPorDificultad($dificultad) {
+        $sql = "SELECT * FROM preguntas
+            WHERE dificultad = ?
+            ORDER BY RAND()";
+        
+        return $this->database->query($sql, [$dificultad]);
+    }
 }
