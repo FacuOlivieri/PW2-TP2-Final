de4cb77dd00b1caf977effe5d2639edc49ee3b99
LGava <lgavagnin074@alumno.unlam.edu.ar>
2026-06-19 02:44:01 -0300

    seguimiento de las preguntas y respuestas

--- model/EstadoPartidaModel.php
+++ model/EstadoPartidaModel.php
@@ -2,7 +2,6 @@
 
 class EstadoPartidaModel
 {
-
     private $database;
 
     public function __construct($database)
@@ -10,8 +9,41 @@ class EstadoPartidaModel
         $this->database = $database;
     }
 
-    public function cargarPreguntaPartidaActualALaBD($idPartidaActual, $idPreguntaActual){
-        $sql = "INSERT INTO partida_preguntas (partida_id, pregunta_id) VALUES (?, ?)";
-        $this->database->execute($sql, [$idPartidaActual,$idPreguntaActual]);
+    public function cargarPreguntaPartidaActualALaBD($idPartidaActual, $idPreguntaActual)
+    {
+        $sql = "INSERT INTO partida_preguntas (partida_id, pregunta_id)
+                VALUES (?, ?)";
+
+        $this->database->execute($sql, [$idPartidaActual, $idPreguntaActual]);
+    }
+
+    public function marcarRespuesta(
+        $idPartida,
+        $idPregunta,
+        $idRespuesta,
+        $esCorrecta,
+        $tiempoRespuesta = null
+    ) {
+        $sql = "UPDATE partida_preguntas
+                SET respuesta_id = ?, es_correcta = ?, tiempo_respuesta = ?
+                WHERE partida_id = ? AND pregunta_id = ?";
+
+        $this->database->execute($sql, [
+            $idRespuesta,
+            $esCorrecta,
+            $tiempoRespuesta,
+            $idPartida,
+            $idPregunta
+        ]);
+    }
+
+    public function obtenerHistorialPartida($idPartida)
+    {
+        $sql = "SELECT *
+                FROM partida_preguntas
+                WHERE partida_id = ?
+                ORDER BY id ASC";
+
+        return $this->database->query($sql, [$idPartida]);
     }
 }
