186b78c90fb2ab9f86d208400a5619f0421e842b
Facu Olivieri <facu.olivieri7@gmail.com>
2026-06-24 13:56:20 -0300

    Vuelta al estado del primer Mailer introducido, limpieza de carpeta .idea
    # Conflicts:
    #	controller/AdministradorController.php

--- controller/AdministradorController.php
+++ controller/AdministradorController.php
@@ -20,51 +20,32 @@ class AdministradorController{
 
 
     public function verDashboard(){
-
         if (!isset($_SESSION["usuario"])) {
             Redirect::to("/usuario/iniciarSesion");
             return;
         }
+
         $this->usuariosModel->requireAdministrador($_SESSION["usuario"]);
 
-        $filtroSeleccionado = $this->request->get("filtrar") ?? "dia";
-        $filtroSeleccionado = $this->validarFiltro($filtroSeleccionado);
 
 
-        $totalJugadores = $this->usuariosModel->cantidadJugadores($filtroSeleccionado);
-        $totalJugadoresNuevos = $this->usuariosModel->cantidadJugadoresNuevos($filtroSeleccionado);
-        $jugadoresPorPais = $this->usuariosModel-> jugadoresPorPais($filtroSeleccionado);
-        $jugadoresPorSexo = $this->usuariosModel->jugadoresPorSexo($filtroSeleccionado);
-        $jugadoresPorEdad = $this->usuariosModel->jugadoresPorEdad($filtroSeleccionado);
-        $partidasJugadas =  $this->partidaModel->cantidadTotalPartidas($filtroSeleccionado);
-        $totalPreguntas = $this->preguntasModel->cantidadTotalPreguntas($filtroSeleccionado);
-        $totalPreguntasCreadas = $this->preguntasModel->cantidadPreguntasCreadas($filtroSeleccionado);
+
+
+
+
+
+
+
+
+
+
 
 
         $this->renderer->render("administradorDashboardView", [
-            "filtro_dia"     => ($filtroSeleccionado === 'dia'),
-            "filtro_semana"  => ($filtroSeleccionado === 'semana'),
-            "filtro_anio"    => ($filtroSeleccionado === 'anio'),
 
-            "totalJugadores" => $totalJugadores,
-            "jugadoresNuevos" => $totalJugadoresNuevos,
-            "jugadoresPorPais" => $jugadoresPorPais,
-            "jugadoresPorSexo" => $jugadoresPorSexo,
-            "jugadoresPorEdad" => $jugadoresPorEdad,
-            "partidasJugadas" => $partidasJugadas,
-            "totalPreguntas" => $totalPreguntas,
-            "totalPreguntasCreadas" => $totalPreguntasCreadas,
 
         ]);
     }
 
-    private function validarFiltro($filtroSeleccionado){
-        $filtrosValidos = ['dia', 'semana', 'anio'];
-        if (!in_array($filtroSeleccionado, $filtrosValidos)) {
-            $filtroSeleccionado = "dia";
-        }
-        return $filtroSeleccionado;
-    }
 
 }
-
